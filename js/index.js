170d5dce2cffe92082348ddce552de3aa8d031e9
akosipaeng <mr.vasquezr@gmail.com>
2023-08-18 20:29:04 +0800

    Add cart js code

---
+++ js/index.js
@@ -0,0 +1,131 @@
+// Cart
+let cartIcon = document.querySelector("#cart-icon");
+let cart = document.querySelector(".cart");
+let closeCart = document.querySelector("#close-cart");
+// Open Cart
+cartIcon.onclick = () => {
+  cart.classList.add("active");
+};
+// Close Cart
+closeCart.onclick = () => {
+  cart.classList.remove("active");
+};
+
+// Cart Working JS
+if (document.readyState == "loading") {
+  document.addEventListener("DOMContentLoaded", ready);
+} else {
+  ready();
+}
+
+function ready() {
+  // remove item from cart
+  let removeCartButton = document.getElementsByClassName("cart-remove");
+  console.log(removeCartButton);
+  for (let i = 0; i < removeCartButton.length; i++) {
+    let button = removeCartButton[i];
+    button.addEventListener("click", removeCartItem);
+  }
+
+  // Quality Changes
+  const qualityInput = document.getElementsByClassName("cart-quantity");
+  for (let i = 0; i < qualityInput.length; i++) {
+    let input = qualityInput[i];
+    input.addEventListener("change", quantityChanged);
+  }
+
+  // Add to Cart
+  const addCart = document.getElementsByClassName("add-cart");
+  for (let i = 0; i < addCart.length; i++) {
+    let button = addCart[i];
+    button.addEventListener("click", addCartClicked);
+  }
+  // Buy Button
+  document
+    .getElementsByClassName("btn-buy")[0]
+    .addEventListener("click", buyButtonClicked);
+}
+
+// Buy button
+function buyButtonClicked() {
+  alert("Your order is placed");
+  let cartContent = document.getElementsByClassName("cart-content")[0];
+  while (cartContent.hasChildNodes()) {
+    cartContent.removeChild(cartContent.firstChild);
+  }
+  updateTotal();
+}
+
+// Remove Items from cart
+function removeCartItem(event) {
+  var buttonClicked = event.target;
+  buttonClicked.parentElement.remove();
+  updateTotal();
+}
+// Quantity changes
+function quantityChanged(event) {
+  let input = event.target;
+  if (isNaN(input.value) || input.value <= 0) {
+    input.value = 1;
+  }
+  updateTotal();
+}
+
+// Add to Cart
+function addCartClicked(event) {
+  const button = event.target;
+  const shopProducts = button.parentElement;
+  let title = shopProducts.getElementsByClassName("product-title")[0].innerText;
+  let price = shopProducts.getElementsByClassName("price")[0].innerText;
+  let productImg = shopProducts.getElementsByClassName("product-img")[0].src;
+  addProductToCart(title, price, productImg);
+  updateTotal();
+}
+function addProductToCart(title, price, productImg) {
+  let cartShopBox = document.createElement("div");
+  cartShopBox.classList.add("cart-box");
+  let cartItems = document.getElementsByClassName("cart-content")[0];
+  let cartItemsNames = cartItems.getElementsByClassName("cart-product-title");
+  for (let i = 0; i < cartItemsNames.length; i++) {
+    if (cartItemsNames[i].innerText == title) {
+      alert("You have already add this item to cart");
+      return;
+    }
+  }
+
+  let cartBoxContent = `
+  <img src="${productImg}" alt="" class="cart-img">
+    <div class="detail-box">
+      <div class="cart-product-title">${title}</div>
+      <div class="cart-price">${price}</div>
+      <input type="number" value="1" class="cart-quantity">
+    </div>
+  <!-- Remove Cart -->
+  <i class='bx bxs-trash-alt cart-remove' ></i>`;
+
+  cartShopBox.innerHTML = cartBoxContent;
+  cartItems.append(cartShopBox);
+  cartShopBox
+    .getElementsByClassName("cart-remove")[0]
+    .addEventListener("click", removeCartItem);
+  cartShopBox
+    .getElementsByClassName("cart-quantity")[0]
+    .addEventListener("change", quantityChanged);
+}
+//Update total
+function updateTotal() {
+  let cartContent = document.getElementsByClassName("cart-content")[0];
+  let cartBoxes = cartContent.getElementsByClassName("cart-box");
+  let total = 0;
+  for (let i = 0; i < cartBoxes.length; i++) {
+    let cartBox = cartBoxes[i];
+    let priceElement = cartBox.getElementsByClassName("cart-price")[0];
+    let quantityElement = cartBox.getElementsByClassName("cart-quantity")[0];
+    let price = parseFloat(priceElement.innerText.replace("$", ""));
+    let quantity = quantityElement.value;
+    total += price * quantity;
+  }
+  // If price contain cents value
+  total = Math.round(total * 100) / 100;
+  document.getElementsByClassName("total-price")[0].innerText = `₱ ${total}`;
+}
